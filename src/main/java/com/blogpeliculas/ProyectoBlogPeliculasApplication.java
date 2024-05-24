97e1b09bc55541267d710d2ba4b9dfcafdb9a01f
Dario Bralo <dariobralo@gmail.com>
2024-05-23 21:08:14 -0300

    Punto de partida

---
+++ src/main/java/com/blogpeliculas/ProyectoBlogPeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.blogpeliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBlogPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBlogPeliculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogpeliculas/ProyectoBlogPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogpeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBlogPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
